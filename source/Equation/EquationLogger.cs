a7ea89fc94167c103d81291e9c998174c7140c47
shlyapp <shlyapp.exe@gmail.com>
2023-02-09 19:42:29 +0400

    Добавление возможности сохранять решения

---
+++ source/Equation/EquationLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equationSolver.Equation
+{
+    public class EquationLogger
+    {
+        private string path;
+
+        public EquationLogger(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetHistory()
+        {
+            List<string> solutions = new List<string>();
+            StreamReader file = new StreamReader(path);
+
+            while (!file.EndOfStream)
+            {
+                solutions.Add(file.ReadLine());
+            }
+
+            file.Close();
+            return solutions;
+        }
+
+        public void AddEquationSolving(string equation, double[] result)
+        {
+            StreamWriter file = new StreamWriter(path, true);
+
+            file.WriteLine($"Уравнение: {equation}\n{result[0]} и {result[1]}\n");
+            file.Close();
+        }
+
+        
+    }
+}
